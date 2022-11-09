92e7de55c3b5630c618fcc166b4fd1898edb0194
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-11-10 00:49:33 +0900

    4자리 랜덤 정답 생성 함수 생성
    1. 숫자야구를 위한 baseball 객체 생성
    2. setPassword 함수 생성
    - 4자리 중복없는 랜덤 숫자 생성하여 숫자야구의 정답으로 사용

--- 숫자야구게임/script.js
+++ 숫자야구게임/script.js
@@ -1,3 +1,47 @@
 ;(function () {
   'use strict'
+
+  const get = (target) => document.querySelector(target)
+  
+  let baseball = {
+    limit: 10,
+    digit: 4,
+    trial : 0,
+    end : false,
+    $answer : get(".ball_answer"),
+    $question : get(".ball_question"),
+    $input :  get(".ball_input"),
+  }
+  const { limit, digit, $question, $answer, $input } = baseball
+  let { trial, end } = baseball
+
+  const init = () =>{
+   
+    get('form').addEventListener('submit',()=>{
+     // playGame()
+    }
+    )
+   setPassword()
+  }
+
+  const setPassword = () =>{
+    let passwordArray = Array(limit).fill(false) //0~9까지 10개의 정수를 표시할 배열 생성
+    let password = ''
+    while(password.length<digit){   //4자리 숫자로 만들기
+      let random = parseInt(Math.random() * 10 ,10) // 0~9 정수 랜덤 생성
+      if(passwordArray[random]){ //true라는 의미는 이미 숫자가 있다는 뜻이므로 랜덤숫자가 중복이라는 의미 continue
+        continue;
+      }
+      password += random
+      console.log(password)
+      passwordArray[random] = true; // 중복 표사를 위한 true 체크
+    }
+
+     baseball.password = password;
+  }
+
+  const playGame = () => {
+
+  }
+  init()
 })()
