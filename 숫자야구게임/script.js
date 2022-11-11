024c16bd6f03cd3a7c22acda0068751c32e10c8b
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-11-12 00:05:31 +0900

    playGame 함수 구현 완료
    playGame 함수 구현
    1. getStrikes - 입력 값과 정답값 비교 후 스트라이크 값 생성
    2. getBalls - 입력 값과 정답값 비교 후 볼 값 생성
    3. 함수 주석 추가

--- 숫자야구게임/script.js
+++ 숫자야구게임/script.js
@@ -42,7 +42,7 @@
   
   const playGame = (e) =>{
     e.preventDefault();
-    if(!!end){
+    if(!!end){ //게임이 끝낫는지 체크
       return ;
     }
     
@@ -56,38 +56,67 @@
     }
     else{     
       trial++
-      if(trial >=limit && !isCorrect(inputNumber , password)){
+      if(trial >=limit && !isCorrect(inputNumber , password)){ // 10번째 시도인데 답이 틀렸을 경우 
         alert("쓰리아웃")
         end = true;
-        inputNumber.value = '';
-        inputNumber.focus();
         $answer.innerHTML = password;
       }
 
-      const result = onPlayed(inputNumber , getResult(inputNumber,password))
-      return $question.innerHTML += `<span>${result}</span>`
+      const result = onPlayed(inputNumber , getResult(inputNumber,password)) 
+       $question.innerHTML += `<span>${result}</span>` // 결과값 출력
     }
+    $input.value = '';
+    $input.focus();
+    
   }
   
-  const isDuplicated = (number) =>{
+  const isDuplicated = (number) =>{ // 중복체크함수
     return [...new Set(number.split(""))].length !== digit; // 입력된 숫자를 배열로 바꾸고 set으로 변형하여 중복체크
   }
 
-  const onPlayed = (number , hint) => {
+  const onPlayed = (number , hint) => { // 정답 체크 후 결과 값 출력 함수
     return `${trial}차 시도 : ${number} , ${hint}`
   }
 
-  const isCorrect = (number , password) =>{
+  const isCorrect = (number , password) =>{ // 정답 확인 함수
     return number === password;
   }
 
-  const getResult = (number , password) => {
-    if(isCorrect(number,password)){
+  const getResult = (number , password) => { // 답 비교 후 결과 값 생성 함수 (홈런 , 스트라이크 , 볼) 
+    if(isCorrect(number,password)){ // 정답일경우 홈런 리턴
       $answer.innerHTML = password;
       end = true;
       return `홈런!`
     }
-    const getStrike = getStrikes();
+    //  정답이 아닐경우 스트라이크 , 볼 결과 리턴
+    const strike = getStrikes(number , password);
+    const ball = getBalls(number , password);
+
+    return `Strike : ${strike} , Ball : ${ball}`
+  }
+
+  const getStrikes = (number, password) => { // 스트라이크값 생성 
+    let strike = 0;
+      number.split("").map((num,index)=>{
+        if(num === password[index]){ // 입력 값과 정답 값이 똑같고 자리까지 똑같음
+          strike++
+        }
+      })
+      return strike;
+  }
+
+  const getBalls = (number, password) => { // 볼값 생성 
+    let ball = 0;
+    const passwordCheckArray = Array(limit).fill(false) 
+    password.split("").map((num) => {
+      passwordCheckArray[num] = true; // 정답 값과 같은 배열INDEX를 TRUE로 변환 , 즉 값이 TRUE이면 정답 번호 중 하나
+    })
+    number.split("").map((num , index)=>{
+      if(passwordCheckArray[num] && num !== password[index]){ // passwordCheckArray[num] 가 TRUE면 정답 번호는 맞음 && num !== password[index] 값이 다르면 자리는 다름
+        ball++
+      }
+    })
+    return ball;
   }
   init()
 })()
