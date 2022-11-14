43d818ee67221dc6c22bf2128ccf79413cb57a60
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-11-15 00:12:04 +0900

    프로그레스 바 구현 완료
    프로그레스 바 구현 완료
    script.js
    1. onscroll - scroll하면 scroll 높이 구해서 width 속성 지정
    2. throttle - 일정하게 시간마다 이벤트 부여

--- 프로그레스바/script.js
+++ 프로그레스바/script.js
@@ -4,4 +4,22 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+  let timeId
+  const throttle = ((callback , time)=>{
+    timeId = setTimeout(()=>{
+      if(timeId) return
+      timeId = undefined
+      callback()
+    },time)
+  })
+  const $progressBar = get('.progress-bar')
+  const onscroll = () =>{
+    const scrollTop = document.documentElement.scrollTop
+    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight
+    const scrollWidth = (scrollTop / height) * 100
+    $progressBar.style.width = scrollWidth + '%'
+  }
+
+  window.addEventListener('scroll',()=>throttle(onscroll(),1000))
+ 
 })()
