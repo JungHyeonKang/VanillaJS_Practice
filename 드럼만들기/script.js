8a0340154d33dba5850174a1ee13b8474f88a8f5
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-11-14 19:53:35 +0900

    드럼만들기 주석추가
    드럼만들기 주석추가

--- 드럼만들기/script.js
+++ 드럼만들기/script.js
@@ -9,8 +9,8 @@
     return document.querySelectorAll(target)
   }
   const keys = Array.from(getAll('.key'))
-  const soundsRoot = 'assets/sounds/'
-  const drumSounds = [
+  const soundsRoot = 'assets/sounds/' // 드럼 사운드 경로
+  const drumSounds = [ // 키보드 키코드와 드럼 사운드 데이터 
     { key: 81, sound: 'clap.wav' },
     { key: 87, sound: 'crash.wav' },
     { key: 69, sound: 'hihat.wav' },
@@ -21,21 +21,21 @@
     { key: 88, sound: 'snare.wav' },
     { key: 67, sound: 'tom.wav' },
   ]
-  const getAudioElement = (index) =>{
+  const getAudioElement = (index) =>{ // 순서대로 오디오 객체 생성
     const audio = document.createElement('audio');
     audio.dataset.key = drumSounds[index].key
     audio.src = soundsRoot + drumSounds[index].sound
     return audio
   }
-  const onMouseDown = (e) =>{
+  const onMouseDown = (e) =>{ // 마우스 클릭시 소리 출력 이벤트
     const keyCode =e.target.getAttribute('data-key')
     playSound(keyCode)
   }
-  const onKeydown = (e) =>{
+  const onKeydown = (e) =>{// 키보드 클릭시 소리 출력 이벤트
     const keyCode =  e.keyCode
     playSound(keyCode)
   }
-  const playSound = (keyCode)=>{
+  const playSound = (keyCode)=>{ // 드럼 사운드 이벤트
     const $audio = get(`audio[data-key="${keyCode}"]`)
     const $key = get(`div[data-key="${keyCode}"]`)
     if($audio && $key){
@@ -44,12 +44,12 @@
       $audio.play()
     }
   }
-  const onTransitionEnd = (e) => {
+  const onTransitionEnd = (e) => { // css 복구
     if (e.propertyName === 'transform') {    
       e.target.classList.remove('playing')
     }
   }
-  const init = () => {
+  const init = () => { // 페이지 진입시 설정 (audio 객체 삽입 , 각 태그에 이벤트 추가 )
     window.addEventListener('keydown',onKeydown)
     keys.forEach((key,index)=>{
       const audio = getAudioElement(index)
