e31e21dd8b22ae99f25fa5a7864214dc77b651fa
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-11-15 20:14:50 +0900

    무한스크롤 구현
    무한스크롤 구현
    1. getPosts - fetch 이용하여 json-server의 데이터 조회
    2. showPost - 비동기로 데이터 출력

--- 무한스크롤/script.js
+++ 무한스크롤/script.js
@@ -4,4 +4,37 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+  const $posts = get('.posts')
+  let currentPage = 1;
+  const limit =10;
+  const getPosts = async () =>{
+    const API_URL = `https://jsonplaceholder.typicode.com/posts`
+    const response =await fetch(API_URL);
+    if(!response.ok) return
+    let posts = await response.json();
+    return posts
+  }
+  const showPosts =  (posts) =>{
+    posts.forEach((post,index)=>{
+      const $post = document.createElement('div')
+      $post.innerHTML = `
+        <div class="header">
+          <div class="id">${post.id}</div>
+          <div class="title">${post.title}</div>
+        </div>
+        <div class="body">${post.body}</div>
+      `
+      $posts.appendChild($post)
+      
+    })
+  }
+  const loadPosts = async (page , limit) =>{
+    let posts = await getPosts();
+    showPosts(posts)
+  }
+
+  window.addEventListener('DOMContentLoaded',()=>{
+    loadPosts(currentPage , limit)
+  })
+  
 })()
