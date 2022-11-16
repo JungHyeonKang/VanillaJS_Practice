46dfa2e0b6f52e2a8dec34035104aaa0d1f245cb
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-11-16 20:41:20 +0900

    캐러셀 만들기
    캐러셀 만들기
    Carousel 클래스 구현
    1 .캐러셀 클래스 생성자 , 초깃값 셋팅
    2. carouselInit - 캐러셀 클래스 이름 초기화
    3.setEventListener - 캐러셀 화살표 버튼 객체 생성 및 이동 함수 클릭이벤트 할당
    4. movePrev , moveNext - 캐러셀 화살표 버튼 클릭시 current 값 변화 함수
    5. moveCarouselTo - current 값을 통해 class 삽입

--- 캐러셀/script.js
+++ 캐러셀/script.js
@@ -5,5 +5,78 @@
     return document.querySelector(target)
   }
 
+  class Carousel{
+    constructor(carouselElement){
+      this.carouselElement = carouselElement
+      this.className = 'carousel_item'
+      this.items = this.carouselElement.querySelectorAll('.carousel_item')
+      this.totalItems = this.items.length
+      
+      this.current = 0;
+      
+    }
   
+    carouselInit(){
+      this.items[0].classList.add('active')
+      this.items[1].classList.add('next')
+      this.items[this.totalItems-1].classList.add('prev')
+    }
+    setEventListener(){
+      this.prevButton = this.carouselElement.querySelector('.carousel_button--prev')
+      this.nextButton = this.carouselElement.querySelector('.carousel_button--next')
+
+      this.prevButton.addEventListener('click', ()=>{
+        this.movePrev()
+      })
+      this.nextButton.addEventListener('click', ()=>{
+        this.moveNext()
+      })
+
+    }
+    movePrev(){
+      if(this.current ===0){
+        this.current = this.totalItems -1 
+      }else{
+        this.current--
+      }
+      moveCarouselTo()
+    }
+    moveNext(){
+      if(this.current === this.totalItems-1){
+        this.current = 0 
+      }else{
+        this.current++
+      }
+      this.moveCarouselTo()
+    }
+    moveCarouselTo(){
+      let prev = this.current -1
+      let next = this.current +1
+
+      if(this.current ===0){
+        prev = this.totalItems-1
+      }else if(this.current === this.totalItems -1){
+        next = 0
+      }
+
+      for(let i=0;i<this.totalItems;i++){
+        if(i === prev){
+          this.items[i].className = this.className + ' prev'
+        }else if(i === next){
+          this.items[i].className = this.className + ' next'
+        }else if(i === this.current){
+          this.items[i].className = this.className + ' active'
+        }else{
+          this.items[i].className = this.className 
+        }
+      }
+    }
+  }
+  
+  window.addEventListener('DOMContentLoaded',()=>{
+    const carouselElement = get('.carousel')
+    const carousel = new Carousel(carouselElement)
+    carousel.setEventListener()
+    carousel.carouselInit()
+  })
 })()
