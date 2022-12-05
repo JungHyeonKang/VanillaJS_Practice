cddf252add71fc49bfe7feb63522dd18b22e76cc
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-06 00:31:13 +0900

    모달창 띄우기
    모달창 띄우기
    - modaltoggle 함수 설정
     -> toggle을 활용하여 show(모달띄우기) , scroll_lock class(화면 backdrop) 설정
    -style.css - show , scroll_lock일때 css 값 설정

--- 모달창만들기/script.js
+++ 모달창만들기/script.js
@@ -3,4 +3,17 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $button = get('.modal_open_button')
+  const $body = get('.body')
+  const $modal = get('.modal')
+
+  $button.addEventListener('click',()=>{
+    toggleModal()
+  })
+
+  const toggleModal = ()=>{
+    $modal.classList.toggle('show')
+    $body.classList.toggle('scroll_lock')
+  }
 })()
