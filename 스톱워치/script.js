af0a97c76be0614bbb991d3518b7f4ad107b0ab1
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-07 00:02:23 +0900

    스톱워치 start 기능 구현
    [스톱워치 start 기능 구현]
    1.stopWatch 클래스를 만들어 구현
     - 생성자를 만들어 스톱워치 객체 기본설정
     - start() - 시작 시간 구한 후 setInterval 이용하여 elapsedTime(지난시간) 구하고 표준시간으로 형변환하여 흐르는 시간 출력

--- 스톱워치/script.js
+++ 스톱워치/script.js
@@ -5,4 +5,45 @@
     return document.querySelector(target)
   }
 
+  class Stopwatch{
+    constructor(element){
+      this.timer = element;
+      this.defaultTime = '00:00.00'
+      this.startTime = 0
+      this.interval = null
+      this.elapsedTime = 0    
+    }
+    start(){
+      clearInterval(this.interval) // 여러번 누르면 interval이 쌓이기 때문에 누를때마다 interval 초기화
+      this.startTime = Date.now() - this.elapsedTime // 시작 시간 구하기
+      this.interval = setInterval(this.startTimer.bind(this),10) // setInterval은 window 객체 this를 사용하므로 클래스 this를 사용하기 위해 명시적으로 this 바인딩
+    }
+    startTimer(){
+      this.elapsedTime = Date.now() - this.startTime //흐른 시간 구하기
+      const time = this.timeToString(this.elapsedTime)
+      this.print(time)
+    }
+    timeToString(time){
+      const date = new Date(time);
+      const minutes = date.getUTCMinutes()
+      const seconds = date.getUTCSeconds()
+      const milliSeconds = date.getUTCMilliseconds()
+      return `${minutes}:${seconds}.${milliSeconds}`
+    }
+    print(text){
+      this.timer.innerHTML = text;
+    }
+  }
+
+  const $timer = get('.timer')
+  const $startButton = get('.timer_button.start')
+  const $stopButton = get('.timer_button.stop')
+  const $resetButton = get('.timer_button.reset')
+
+  const stopwatch = new Stopwatch($timer)
+
+  $startButton.addEventListener('click', () => {
+    stopwatch.start()
+  })
+
 })()
