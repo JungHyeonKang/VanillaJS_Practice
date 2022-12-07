63ef07780e54745d53bf33fc8ee7990d66af9194
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-08 00:22:00 +0900

    계산기 기본셋팅
    계산기 구현을 위한 css , html 기본셋팅

---
+++ 계산기/script.js
@@ -0,0 +1,12 @@
+;(function () {
+  'use strict'
+
+  const get = (target) => {
+    return document.querySelector(target)
+  }
+
+  const getAll = (target) => {
+    return document.querySelectorAll(target)
+  }
+  
+})()
