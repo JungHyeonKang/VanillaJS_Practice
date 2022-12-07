de377de12129b2ea0146f2e5ebabc9fe0261ac5f
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-08 00:45:29 +0900

    계산기 클래스 기본설정
    생성자 생성하여 기본설정 완료
    dom 객체 생성

--- 계산기/script.js
+++ 계산기/script.js
@@ -9,4 +9,17 @@
     return document.querySelectorAll(target)
   }
   
+  class Calculator{
+    constructor(element){
+      this.element = element
+      this.currentValue = ''
+      this.prevValue = ''
+      this.operation = null
+    }
+  }
+
+  const display = get('.display')
+  const numberButtons = getAll('.cell_button.number')
+  const calculator = new Calculator(display)
+  
 })()
