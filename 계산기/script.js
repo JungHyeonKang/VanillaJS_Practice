ee041eec42596010449a5c0a8be692383cbb8ab4
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-08 01:12:58 +0900

    계산기 연산자 입력 기능 추가
    setOperation - 연산자 키 누르면 연산을 위해 현재 입력 값 prevValue 로 이동 후 초기화
    누른 연산자키에 active 클래스 추가하여 css 변경
    updateDisplay - 연산자 키 누를시 입력한 숫자값 그대로 input태그에 보여지기 위해 분기 처리

--- 계산기/script.js
+++ 계산기/script.js
@@ -16,22 +16,46 @@
       this.prevValue = ''
       this.operation = null
     }
+
     appendNumber(number){
       if(number === '.' && this.currentValue.includes('.')) return
       this.currentValue = this.currentValue + number.toString()
     }
-    updateDisplay(){
-      this.element.value = this.currentValue
+
+    updateDisplay(){  
+      if(this.currentValue){ // 연산자 키 누르면 숫자 안지워지기 위해 설정
+        this.element.value = this.currentValue
+      }
+      
+    }
+
+    setOperation(operation){
+      this.operation = operation
+      this.prevValue = this.currentValue
+      this.currentValue = ''
+      const elements = Array.from(getAll('.cell_button.operation')) 
+      const element =elements.filter((element) =>
+        element.innerText.includes(this.operation)
+      )[0]
+      element.classList.add('active')
     }
   }
 
   const display = get('.display')
   const numberButtons = getAll('.cell_button.number')
+  const operationButtons = getAll('.cell_button.operation')
   const calculator = new Calculator(display)
   
   numberButtons.forEach(button=>{
     button.addEventListener('click',()=>{
       calculator.appendNumber(button.innerText)
+      calculator.updateDisplay()
+    })
+  })
+
+  operationButtons.forEach(button =>{
+    button.addEventListener('click',()=>{
+      calculator.setOperation(button.innerText)
       calculator.updateDisplay()
     })
   })
