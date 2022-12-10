09f30ecd72ad19300e2efe66ee4077dfe2276d00
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-11 02:11:38 +0900

    카카오맵 띄우기
    defaultPos - 기본 위치 설정 (판교테크노밸리)
    mapContainer - 설정한 위치로 카카오맵 객체 생성

--- 카카오지도 사용하기/script.js
+++ 카카오지도 사용하기/script.js
@@ -5,5 +5,16 @@
     return document.querySelector(target)
   }
 
+  const defaultPos = { //기본 위치는 판교테크노밸리로 설정
+    lat: 37.4020589,
+    lng: 127.1064401,
+  }
+  const $map = get('#map')
+  
+  const mapContainer = new kakao.maps.Map($map,{
+    center: new kakao.maps.LatLng(defaultPos.lat, defaultPos.lng), //지도의 중심좌표.
+	  level: 3 //지도의 레벨(확대, 축소 정도)
+  })
+
   
 })()
