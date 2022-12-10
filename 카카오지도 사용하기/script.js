ca4fd9b5d458ac7a4e8fe37179b8b64d59c9f5e6
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-11 02:44:39 +0900

    카카오맵에 현재 접속 위치 이동 및 마커 표시 기능추가
    getLocation - 위치 정보 서비스인지 확인 후 지원하다면 현재 위치 구하는 함수 실행
    successGeolocation - 현재 위치값을 얻었다면 그 위치로 이동 후 그 위치에 마커 표시하도록 기능 추가
    errorGeolocation - 현재 위치값 찾기 예외처리 함수

--- 카카오지도 사용하기/script.js
+++ 카카오지도 사용하기/script.js
@@ -31,6 +31,7 @@
     lng: 127.1064401,
   }
   const $map = get('#map')
+  const geoLocationButton = get('.geolocation_button')
   
   const mapContainer = new kakao.maps.Map($map,{
     center: new kakao.maps.LatLng(defaultPos.lat, defaultPos.lng), //지도의 중심좌표.
@@ -64,7 +65,44 @@
       infowindow.open(mapContainer, marker); 
     })
   }
+  const getLocation = ()=>{
+    if (navigator.geolocation) {
+    
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition( successGeolocation,errorGeolocation)
+        
+      
+  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      
+      let locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
+          message = 'geolocation을 사용할수 없어요..'
+          
+     // displayMarker(locPosition, message);
+  }
+  }
+  const successGeolocation = (position) =>{
+    var lat = position.coords.latitude; // 위도
+    var lon = position.coords.longitude; // 경도
+    var locPosition = new kakao.maps.LatLng(lat, lon)
+    mapContainer.setCenter(locPosition);
+    const marker =createMarker(lat ,lon)
+    marker.setMap(mapContainer)
+  } 
+
+  const errorGeolocation = (error) => {
+    if (error.code == 1) {
+      alert('위치 정보를 허용해주세요.')
+    } else if (error.code == 2) {
+      alert('사용할수 없는 위치 입니다.')
+    } else {
+      alert('오류가 발생했습니다.')
+    }
+  }
   const init = () => {
+    geoLocationButton.addEventListener('click', () => {
+      getLocation()
+    })
+    
     createShopElement()
   }
   init()
