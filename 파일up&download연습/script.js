5b0e3b56a8e3c1831f84aa7138afd5bd4427680d
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-12 20:45:09 +0900

    파일 업로드 기능 구현
    inputfile 태그에 파일을 드래그앤드롭하여 업로드 되도록 구현

--- 파일up&download연습/script.js
+++ 파일up&download연습/script.js
@@ -8,8 +8,18 @@
       this.container = get('main')
       this.inputFile = get('.drag_area input')
       this.canvas =get('.canvas')
+      this.fileImage = get('.fileImage')
       this.btnSave = get('.btn_save')
+      this.fileUpload()
     }
+
+    fileUpload(){
+      this.inputFile.addEventListener('change',(e)=>{
+       const fileURL = URL.createObjectURL(e.target.files[0])
+       this.fileImage.setAttribute('src',fileURL)
+      })
+    }
+
   }
   new FileUploader()
 })()
