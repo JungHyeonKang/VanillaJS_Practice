ab62b0f255f915051b1f07e515a146f48eb393cd
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-12 21:01:08 +0900

    파일 다운로드 기능 추가
    fileDownload - 다운로드 할 이미지 URL 구하고 <a> 태그 생성하여 href 속성으로 설정. 자동 클릭하게 하여 파일 다운로드.

--- 파일up&download연습/script.js
+++ 파일up&download연습/script.js
@@ -11,15 +11,30 @@
       this.fileImage = get('.fileImage')
       this.btnSave = get('.btn_save')
       this.fileUpload()
+      this.clickEvent()
     }
-
+    clickEvent(){
+      this.btnSave.addEventListener('click',this.fileDownload.bind(this))
+    }
     fileUpload(){
       this.inputFile.addEventListener('change',(e)=>{
        const fileURL = URL.createObjectURL(e.target.files[0])
        this.fileImage.setAttribute('src',fileURL)
       })
     }
+    fileDownload(){
+      const fileName = this.canvas.toDataURL()
+      const filedownloader = document.createElement('a')
+      filedownloader.style.display = 'none'
+      filedownloader.setAttribute('href', fileName)
+      filedownloader.setAttribute('download', 'test.png')
+      this.container.appendChild(filedownloader)
+      filedownloader.click()
+      setTimeout(()=>{
+        this.container.removeChild(filedownloader)
+      },100)
 
+    }
   }
   new FileUploader()
 })()
