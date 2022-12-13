ae9cb583dd10775ac18d917f1218340faf336f28
JungHyeonKang <rkdwjdgus0103@gmail.com>
2022-12-13 20:55:29 +0900

    글 편집 기능 구현
    commands - 글 편집 기능들 배열 선언
    init - 선언한 글편집 속성 버튼 생성 및 클릭 이벤트 할당
    commandObject - 선언한 속성 이름을 key로 value 를 속성으로 하여 객체로 변환
    doCommand - execCommand 메서드 사용하여 속성 이름에 따라 글 편집 기능하도록 구현

--- 글쓰기에디터/script.js
+++ 글쓰기에디터/script.js
@@ -5,5 +5,90 @@
     return document.querySelector(target)
   }
 
-  
+  const commands = [
+    {
+      cmd: 'backColor',
+      val: 'red',
+      label: '배경 컬러',
+    },
+    {
+      cmd: 'bold',
+      label: '굵기',
+    },
+    {
+      cmd: 'delete',
+      label: '삭제',
+    },
+    {
+      cmd: 'fontSize',
+      label: '폰트 사이즈',
+      val: '1-7',
+    },
+    {
+      cmd: 'foreColor',
+      label: '폰트 컬러',
+      val: 'rgba(0,0,0,.5)',
+    },
+    {
+      cmd: 'insertImage',
+      label: '이미지 추가',
+      val: 'http://dummyimage.com/160x90',
+    },
+    {
+      cmd: 'italic',
+      label: '기울이기',
+    },
+    {
+      cmd: 'justifyCenter',
+      label: '중앙 정렬',
+    },
+    {
+      cmd: 'justifyFull',
+      label: '양쪽 정렬',
+    },
+    {
+      cmd: 'justifyLeft',
+      label: '좌측 정렬',
+    },
+    {
+      cmd: 'justifyRight',
+      label: '우측 정렬',
+    },
+
+    {
+      cmd: 'selectAll',
+      label: '전체 선택',
+    },
+    {
+      cmd: 'underline',
+      label: '밑줄',
+    },
+    {
+      cmd: 'undo',
+      label: '취소',
+    },
+  ]
+
+  const editbuttons = get('.editor_buttons')
+
+  const commandObject = {}
+  const doCommand = (cmdKey)=>{
+    const cmd = commandObject[cmdKey]
+    console.log(cmd)
+    const val = cmd.val ? prompt('입력해주세요' , cmd.val) : ''
+    document.execCommand(cmd.cmd , false , val || '')
+  }
+  const init = () =>{
+    commands.map((command)=>{
+      commandObject[command.cmd] = command
+      const element = document.createElement('button')
+      element.innerText = command.label
+      element.addEventListener('click',(e)=>{
+        e.preventDefault()
+        doCommand(command.cmd)
+      })
+      editbuttons.appendChild(element)
+    })
+  }
+  init()
 })()
